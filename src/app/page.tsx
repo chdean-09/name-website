e5e4d88eeb770d4ddc77f8f839a6eccee51e0cb2
chdean-09 <chadandrada20@gmail.com>
2025-07-05 16:58:38 +0800

    introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,174 @@
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  // const features = [
+  //   {
+  //     name: 'Push to deploy.',
+  //     description:
+  //       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+  //     icon: Lock,
+  //   },
+  //   {
+  //     name: 'SSL certificates.',
+  //     description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+  //     icon: BellRing,
+  //   },
+  //   {
+  //     name: 'Database backups.',
+  //     description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+  //     icon: CalendarCheck,
+  //   },
+  // ]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const words = [
+    {
+      text: "N.A.M.E",
+      className: "text-blue-500 dark:text-blue-500 text-4xl md:text-[6rem] font-bold mt-1 leading-none",
+    },
+  ];
+
+  return (
+    <div className="overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="-mt-10 text-4xl font-semibold text-black dark:text-white">
+              Introducing <br />
+              <TypewriterEffect words={words} />
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={`/logo.png`}
+          alt=""
+          height={720}
+          width={1400}
+          className="bg-white mx-auto rounded-2xl object-contain h-full object-center p-20"
+          draggable={false}
+        />
+      </ContainerScroll>
+      <div className="pb-24 sm:pb-32 -mt-12 sm:mt-0">
+        <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-center text-base/7 font-semibold text-indigo-600">Smart & Secure</h2>
+          <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance sm:text-5xl">
+            Everything you need for a smarter door lock
+          </p>
+          <p className="mt-4 text-center text-base text-gray-600 dark:text-gray-400">
+            Powered by <strong className="text-gray-900 dark:text-white">N.A.M.E</strong> — <em>
+              <span className="font-bold text-gray-900 dark:text-white">N</span>ext-gen {" "}
+              <span className="font-bold text-gray-900 dark:text-white">A</span>ccess {" "}
+              <span className="font-bold text-gray-900 dark:text-white">M</span>anagement {" "}
+              <span className="font-bold text-gray-900 dark:text-white">E</span>ntry
+              </em>
+          </p>
+
+          <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+            {/* Mobile Control */}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg lg:rounded-l-4xl" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)] bg-indigo-100 dark:bg-indigo-900/80">
+                <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-900 dark:text-white max-lg:text-center">
+                    Mobile Control
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-700 dark:text-gray-300 max-lg:text-center">
+                    Lock and unlock your door from anywhere using your smartphone. Real-time status updates and intuitive UI.
+                  </p>
+                </div>
+                <div className="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                  <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[8cqw] border-x-[2cqw] border-t-[2cqw] border-black shadow-2xl">
+                    <Image
+                      width={700}
+                      height={700}
+                      alt=""
+                      src="/app-picture.jpg"
+                      className="size-full object-cover object-top"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-black/5 lg:rounded-l-4xl" />
+            </div>
+
+            {/* Real-Time Sync */}
+            <div className="relative max-lg:row-start-1">
+              <div className="absolute inset-px rounded-lg max-lg:rounded-t-4xl" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] bg-cyan-100 dark:bg-cyan-900/80">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-900 dark:text-white max-lg:text-center">
+                    Real-Time Sync
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-700 dark:text-gray-300 max-lg:text-center">
+                    WebSockets keep your device state and mobile app perfectly in sync—no refresh needed.
+                  </p>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
+                  <img
+                    alt="Real-Time Performance"
+                    src="/realtime-sync.png"
+                    className="w-full max-lg:max-w-xs"
+                  />
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-black/5 max-lg:rounded-t-4xl" />
+            </div>
+
+            {/* Trusted Security */}
+            <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+              <div className="absolute inset-px rounded-lg" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] bg-rose-100 dark:bg-rose-900/80">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-900 dark:text-white max-lg:text-center">
+                    Trusted Security
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-700 dark:text-gray-300 max-lg:text-center">
+                    Secure pairing, HTTPS communication, and user-specific access ensure your home is always protected.
+                  </p>
+                </div>
+                <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
+                  <img
+                    alt="Security"
+                    src="/security-esp.png"
+                    className="h-[min(152px,40cqw)] object-cover"
+                  />
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-black/5" />
+            </div>
+
+            {/* Seamless Integration */}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg max-lg:rounded-b-4xl lg:rounded-r-4xl" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)] bg-emerald-100 dark:bg-emerald-900/80">
+                <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-900 dark:text-white max-lg:text-center">
+                    Seamless Integration
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-700 dark:text-gray-300 max-lg:text-center">
+                    Easily connect your smart lock to your home Wi-Fi and mobile devices. Setup takes minutes with Bluetooth pairing and auto-network discovery.
+                  </p>
+                </div>
+                <div className="@container relative min-h-120 w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                  <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[8cqw] border-x-[2cqw] border-t-[2cqw] border-black shadow-2xl">
+                    <Image
+                      width={700}
+                      height={700}
+                      alt=""
+                      src="/app-picture.jpg"
+                      className="size-full object-cover object-top"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-black/5 max-lg:rounded-b-4xl lg:rounded-r-4xl" />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
     </div>
   );
 }
